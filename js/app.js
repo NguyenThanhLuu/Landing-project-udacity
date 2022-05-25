7e08b8f948b22f3c1dc79e8551a6e444c2e6ecec
nguyen thanh luu <nguyenthanhluu181097@gmail.com>
2022-05-25 22:04:44 +0700

    Implement code fix js file and readme

--- js/app.js
+++ js/app.js
@@ -1,135 +1,40 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
+// Declare global variable
 var navbarContainer = document.getElementById("navbarList");
+
+// Build navbar add four li child tag into ul tag
 for (let i = 0; i < 4; i++) {
     var newElement = document.createElement("li");
-    newElement.className = `section${i + 1}`; 
+    newElement.className = `menu__link section${i + 1}`; 
     newElement.innerHTML = `Section ${i + 1}`;
     navbarContainer.appendChild(newElement);
 }
-// console.log('this testss', document.getElementById("navbarList"));
 
-
-document.querySelector('.section1').addEventListener('click', function() {
-  document.querySelector('#section1').scrollIntoView();
-})
-
-document.querySelector('.section2').addEventListener('click', function() {
-  document.querySelector('#section2').scrollIntoView();
-})
-
-document.querySelector('.section3').addEventListener('click', function() {
-  document.querySelector('#section3').scrollIntoView();
-})
-
-document.querySelector('.section4').addEventListener('click', function() {
-  document.querySelector('#section4').scrollIntoView();
-})
-
-var elementList = navbarContainer.getElementsByClassName("menu__link");
-console.log(elementList);
-
-for (let i = 0; i < elementList.length; i++) {
-    elementList[i].addEventListener("click", function() {
-
-
-        // if (this.className.split(' ')[1] == 'active') {
-        //     return;
-        // } else {
-        //     this.className += " active";
-        // }
-        // var elHasActiveClass = navbarContainer.getElementsByClassName("active");
-
-        // if (elHasActiveClass.length > 1) {
-        //     if (this !== elHasActiveClass[0]) {
-        //         elHasActiveClass[0].className = elHasActiveClass[0].className.replace(" active", "")
-        //     } else {
-        //         elHasActiveClass[1].className = elHasActiveClass[0].className.replace(" active", "")
-        //     }
-        // }
-    })
-}
-console.log(document.getElementsByTagName('h1'))
-
+// Write function to scroll to section it will be active
 function makeActive() {
     const sections = document.querySelectorAll('section');
-    console.log('this sections:', sections)
     for (const section of sections) {
       const box = section.getBoundingClientRect();
       const sectionValueId = section.getAttribute('id');
-      // You can play with the values in the "if" condition to further make it more accurate. 
       if (box.top <= 150 && box.bottom >= 150) {
           section.className = "active-section";
-        //   console.log('hehehe', document.getElementsByClassName(sectionValueId));
           document.getElementsByClassName(sectionValueId)[0].className = `menu__link ${sectionValueId} active`
-          console.log('sdfsdf', document.getElementsByClassName(sectionValueId))
       } else {
           section.className = "";
           document.getElementsByClassName(sectionValueId)[0].className = `menu__link ${sectionValueId}`;
-        // Remove active state from other section and corresponding Nav link.
       }
     }
   }
 
-  // Make sections active
+// Make sections active
 document.addEventListener("scroll", function() {
     makeActive();
-  });
+});
 
-// document.addEventListener('scroll', checkScroll);
-// checkScroll();
-
+// Active section when click name section on navbar
+var classListNavElement = document.getElementsByClassName('menu__link');
+for (let i = 0; i < classListNavElement.length; i++) {            
+  classListNavElement[i].addEventListener('click',function(){
+    console.log(true);
+    document.querySelector(`#section${i + 1}`).scrollIntoView({ behavior: "smooth" });
+  })
+} 
